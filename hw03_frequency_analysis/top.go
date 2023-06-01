9955cdf8a86d87d395dd0fd22ff48da5a776152d
Adnrey <vetrov@linkage.ru>
2023-06-01 18:31:48 +0200

    hw03: upd top, test

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,49 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type KeyValue struct {
+	key   string
+	value int
+}
+
+func Top10(text string) []string {
+	text = strings.ToLower(text)
+	re := regexp.MustCompile(`[1-9a-zа-яё]+(?:-?[а-яё]+)*`)
+	words := re.FindAllString(text, len(text)+1)
+
+	mapWords := map[string]int{}
+	for _, word := range words {
+		mapWords[word]++
+	}
+
+	var sliceWords []KeyValue
+	for key, word := range mapWords {
+		sliceWords = append(sliceWords, KeyValue{key: key, value: word})
+	}
+
+	sort.Slice(sliceWords, func(i, j int) bool {
+		if sliceWords[i].value != sliceWords[j].value {
+			return sliceWords[i].value > sliceWords[j].value
+		}
+		return sliceWords[i].key < sliceWords[j].key
+	})
+	fmt.Println(sliceWords)
+
+	sliceLen := len(sliceWords)
+	if sliceLen > 10 {
+		sliceLen = 10
+	}
+
+	result := make([]string, 0)
+	for i := 0; i < sliceLen; i++ {
+		result = append(result, sliceWords[i].key)
+	}
+
+	return result
 }
