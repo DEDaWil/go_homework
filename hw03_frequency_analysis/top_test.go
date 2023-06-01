f80b985391b2e8091d653ef2d55db55004d1e9ae
Adnrey <vetrov@linkage.ru>
2023-06-01 18:37:09 +0200

    hw03: fix test

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -40,9 +40,22 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
-var textEng = `This tale grew in the telling, until it became a history of the Great War of the Ring and included many glimpses of the yet more ancient history that preceded it. It was begun soon afterThe Hobbit was written and before its publication in 1937; but I did not go on with this sequel, for I wished first to complete and set in order the mythology and legends of the Elder Days, which had then been taking shape for some years. I desired to do this for my own satisfaction, and I had little hope that other people would be interested in this work, especially since it was primarily linguistic in inspiration and was begun in order to provide the necessary background of 'history' for Elvish tongues.
+var textEng = `This tale grew in the telling, until it became a history of the Great War of the Ring and included 
+	many glimpses of the yet more ancient history that preceded it. It was begun soon afterThe Hobbit was written 
+	and before its publication in 1937; but I did not go on with this sequel, for I wished first to complete and 
+	set in order the mythology and legends of the Elder Days, which had then been taking shape for some years. I 
+	desired to do this for my own satisfaction, and I had little hope that other people would be interested in 
+	this work, especially since it was primarily linguistic in inspiration and was begun in order to provide the 
+	necessary background of 'history' for Elvish tongues.
 
-When those whose advice and opinion I sought correctedlittle hope tono hope, I went back to the sequel, encouraged by requests from readers for more information concerning hobbits and their adventures. But the story was drawn irresistibly towards the older world, and became an account, as it were, of its end and passing away before its beginning and middle had been told. The process had begun in the writing ofThe Hobbit, in which there were already some references to the older matter: Elrond, Gondolin, the High-elves, and the orcs, as well as glimpses that had arisen unbidden of things higher or deeper or darker than its surface: Durin, Moria, Gandalf, the Necromancer, the Ring. The discovery of the significance of these glimpses and of their relation to the ancient histories revealed the Third Age and its culmination in the War of the Ring.`
+	When those whose advice and opinion I sought correctedlittle hope tono hope, I went back to the sequel, 
+	encouraged by requests from readers for more information concerning hobbits and their adventures. But the 
+	story was drawn irresistibly towards the older world, and became an account, as it were, of its end and 
+	passing away before its beginning and middle had been told. The process had begun in the writing ofThe Hobbit, 
+	in which there were already some references to the older matter: Elrond, Gondolin, the High-elves, and the orcs, 
+	as well as glimpses that had arisen unbidden of things higher or deeper or darker than its surface: Durin, Moria, 
+	Gandalf, the Necromancer, the Ring. The discovery of the significance of these glimpses and of their relation to 
+	the ancient histories revealed the Third Age and its culmination in the War of the Ring.`
 
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
